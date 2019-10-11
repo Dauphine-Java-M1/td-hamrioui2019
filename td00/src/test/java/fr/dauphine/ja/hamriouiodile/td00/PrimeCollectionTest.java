a5e0eed8c5a8860688320d9bbd210b1aef73b62f
hamrod19 <hamrod19@b032-08.crio.dauphine.fr>
2019-10-11 13:54:18 +0200

    I added the new class PrimeJavaCollectionTest.java in my git

---
+++ td00/src/test/java/fr/dauphine/ja/hamriouiodile/td00/PrimeCollectionTest.java
@@ -0,0 +1,41 @@
+package fr.dauphine.ja.hamriouiodile.td00;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+/**
+ * TO EDIT THE NAME OF THE CLASS LET'S JUST USE ALT+SHIFT+R
+ * */
+public class PrimeCollectionTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public PrimeCollectionTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( PrimeCollectionTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}

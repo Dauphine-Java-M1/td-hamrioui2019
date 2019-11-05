bab695dfd6b350069cf031a05512308df534756d
hamrod19 <hamrod19@b028-28.crio.dauphine.fr>
2019-11-05 13:34:23 +0100

    11-05-19

--- shapes/src/main/java/fr/dauphine/ja/hamriouiodile/shapes/Circle.java
+++ shapes/src/main/java/fr/dauphine/ja/hamriouiodile/shapes/Circle.java
@@ -10,27 +10,56 @@ public class Circle {
 	}
 	public Circle() {}
 	public String toString() {
-		return "c est un cercle de centre "+this.centre.afficher()+" et de rayon "+this.rayon;
+		return "c est un cercle de centre "+this.centre.afficher()+" de rayon "+this.rayon+" et une surface de : "+this.surface();
 	}
-	static Circle translate (Circle c, int dx, int dy)
+	
+	public Circle translate (int dx, int dy)
     {
-       Circle c1 = new Circle ();
-       int x=c.centre.getX() + dx;
-       int y=c.centre.getY() + dy;
-       c1.centre.setX(x);
-       c1.centre.setY(y);
-       return c1;
+       int x=this.centre.getX() + dx;
+       int y=this.centre.getY() + dy;
+       this.centre.setX(x);
+       this.centre.setY(y);
+       return this;
     }
 	public Point getCenter() {
 		return this.centre; 
-		
 	}
+	public double surface() {
+		return Math.PI*rayon*rayon;
+	}
+	/*
+	 * public boolean contains(Point p) {
+	je vais calculer la distance des coordonnées par rapport au centre puis j'utilise la distance euclédienne 
+		int x=p.getX()-this.centre.getX();
+		int y=p.getY()-this.centre.getY();
+		double d=Math.pow(x, 2)+Math.pow(y, 2);
+		return  
+	}*/
+	
+	/*
+	 * public boolean contains(Point p, liste ){
+	 * 
+	 * 
+	 * }
+	 * 
+	 * 
+	 * 
+	 */
 	 public static void main( String[] args ) {
+		/*Point p=new Point(1,2);
+		 Circle c=new Circle(p,1);
+		 Circle c2=new Circle(p,2);
+		 c.translate(1,1);
+		 System.out.println(c.toString()+" "+c); */
+		/*
+		 * question 6
+		 Circle c=new Circle(p,1);
+		 c.translate(1,1);
+		 System.out.println(c);*/
 		 Point p=new Point(1,2);
 		 Circle c=new Circle(p,1);
 		 Circle c2=new Circle(p,2);
-		// translate(c2,1,1);
-		 System.out.println(c.toString()+" "+c2); 
+		 c2.translate(1,1);
+		 System.out.println(c+" "+c2);
 	 }
-	
 }
